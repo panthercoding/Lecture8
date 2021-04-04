cf28962863e31eec4366924701bc83567ec87d7e
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-04-04 17:34:25 -0500

    Create infinity.java

---
+++ infinity.java
@@ -0,0 +1,11 @@
+public class infinity{
+
+  //why does this method suck?
+  public static void doSomething(){
+    doSomething();
+  }
+
+  public static void main(String[] args){
+    doSomething();
+  }
+}
